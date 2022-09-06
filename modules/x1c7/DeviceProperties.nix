359e2517058f6448d9fc29cdaf1456a333767cae
Harry Ying <lexugeyky@outlook.com>
2022-09-06 11:18:08 +0800

    patch display connector type

--- modules/x1c7/DeviceProperties.nix
+++ modules/x1c7/DeviceProperties.nix
@@ -8,6 +8,16 @@
         "framebuffer-patch-enable" = mkData "AQAAAA==";
         "framebuffer-stolenmem" = mkData "AAAwAQ==";
         "framebuffer-fbmem" = mkData "AACQAA==";
+        "enable-hdmi20" = mkData "AQAAAA==";
+        "framebuffer-con0-enable" = mkData "AQAAAA==";
+				"framebuffer-con0-alldata" = mkData "AAAIAAIAAACYAAAA";
+        "framebuffer-con1-enable" = mkData "AQAAAA==";
+				"framebuffer-con1-alldata" = mkData "AQEJAAAIAACHAQAA";
+        "framebuffer-con2-enable" = mkData "AQAAAA==";
+				"framebuffer-con2-alldata" = mkData "AgQKAAAEAACHAQAA";
+        # Patch the ports' connector-type to HDMI
+        # "framebuffer-con1-type" = mkData "AAgAAA=="; # <00080000>
+        # "framebuffer-con2-type" = mkData "AAgAAA=="; # <00080000>
       };
     };
     Delete = { };
